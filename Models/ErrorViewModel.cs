5b2d1871cfbf8a28cece260ec082c0804f08aa7b
Doruk Özüdoğru <doruk.ozudogru@oplog.com.tr>
2019-08-26 20:55:24 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TemplateProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
